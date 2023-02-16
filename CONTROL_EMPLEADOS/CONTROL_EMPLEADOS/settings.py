73613e0968899a31559aaadd273ff4d097b08867
David Magán <122270411+davidmaganfr@users.noreply.github.com>
2023-02-16 21:24:54 +0100

    Update settings.py

--- CONTROL_EMPLEADOS/CONTROL_EMPLEADOS/settings.py
+++ CONTROL_EMPLEADOS/CONTROL_EMPLEADOS/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'company',
         'USER': 'postgres',
-        'PASSWORD': 'Da08680774',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
